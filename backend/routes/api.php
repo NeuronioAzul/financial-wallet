d72a6467037be1639342510918405a3c69b6ee81
Mauro Rocha <mauro.rocha.t@gmail.com>
2025-11-15 20:14:25 -0300

    feat(routes): add endpoints for managing addresses and documents

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -24,6 +24,24 @@ Route::prefix('v1')->middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
     Route::get('/me', [\App\Http\Controllers\Api\AuthController::class, 'me']);
 
+    // Profile
+    Route::get('/profile', [\App\Http\Controllers\Api\ProfileController::class, 'show']);
+    Route::put('/profile', [\App\Http\Controllers\Api\ProfileController::class, 'update']);
+
+    // Addresses
+    Route::get('/addresses', [\App\Http\Controllers\Api\AddressController::class, 'index']);
+    Route::post('/addresses', [\App\Http\Controllers\Api\AddressController::class, 'store']);
+    Route::get('/addresses/{address}', [\App\Http\Controllers\Api\AddressController::class, 'show']);
+    Route::put('/addresses/{address}', [\App\Http\Controllers\Api\AddressController::class, 'update']);
+    Route::delete('/addresses/{address}', [\App\Http\Controllers\Api\AddressController::class, 'destroy']);
+
+    // Documents
+    Route::get('/documents', [\App\Http\Controllers\Api\DocumentController::class, 'index']);
+    Route::post('/documents', [\App\Http\Controllers\Api\DocumentController::class, 'store']);
+    Route::get('/documents/status', [\App\Http\Controllers\Api\DocumentController::class, 'status']);
+    Route::get('/documents/{document}', [\App\Http\Controllers\Api\DocumentController::class, 'show']);
+    Route::delete('/documents/{document}', [\App\Http\Controllers\Api\DocumentController::class, 'destroy']);
+
     // Wallets
     Route::get('/wallet', [\App\Http\Controllers\Api\WalletController::class, 'show']);
     Route::get('/wallet/balance', [\App\Http\Controllers\Api\WalletController::class, 'balance']);
