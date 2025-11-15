132e1f5460760466c0823da51ddf755bfe290741
Mauro Rocha <mauro.rocha.t@gmail.com>
2025-11-15 19:38:55 -0300

    feat(enums): add DocumentStatus and DocumentType enums with labels

---
+++ backend/app/Enums/DocumentType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Enums;
+
+enum DocumentType: string
+{
+    case PHOTO = 'photo';
+    case RG_FRONT = 'rg_front';
+    case RG_BACK = 'rg_back';
+    case CNH_FRONT = 'cnh_front';
+    case CNH_BACK = 'cnh_back';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::PHOTO => 'Foto (Selfie)',
+            self::RG_FRONT => 'RG - Frente',
+            self::RG_BACK => 'RG - Verso',
+            self::CNH_FRONT => 'CNH - Frente',
+            self::CNH_BACK => 'CNH - Verso',
+        };
+    }
+}
